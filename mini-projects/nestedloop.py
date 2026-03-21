1555b380f5ff207c92b77ed7ae36fdf28e8b45d8
Roshan.R <roshanleo953@gmail.com>
2026-03-21 21:00:40 +0530

    Add nested loop and multiplication table functionality

---
+++ mini-projects/nestedloop.py
@@ -0,0 +1,19 @@
+for i in range(1,6):
+    for j in range(1,6):
+        print("*", end=" ")
+    print()
+
+
+num = int(input("Enter a number: "))
+for i in range(1,11):
+    print(num, "x", i, "=", num*i)
+
+
+rows = int(input("Enter rows: "))
+
+for i in range(1, rows+1):
+    for j in range(1, i+1):
+        print("*", end=" ")
+    print()
+
+
