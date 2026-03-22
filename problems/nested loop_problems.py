3f55c3f92230314298f854d2f13c3ec3686b26fb
Roshan.R <roshanleo953@gmail.com>
2026-03-23 05:02:53 +0530

    Create nested loop_problems.py

---
+++ problems/nested loop_problems.py
@@ -0,0 +1,36 @@
+for i in range (1 , 4):
+    for j in range (1 , i+1):
+        print(j , end=" ")
+    print()    
+    
+    
+for i in range(1, 4):
+    for j in range (1 ,i+1):
+        print(i, end=" ")
+    print()    
+    
+    
+for i in range (1 , 4):
+    for j in range (i):
+        print(j ,end=" ")
+    print()
+    
+    
+for i in range(1, 4):
+    for j in range(i): 
+        print(5, end=" ")
+    print()
+
+    
+n = 1
+for i in range (1,4):
+    for j in range (i):
+        print(n, end=" ")
+        n = n+1
+    print()
+    
+    
+for i in range(1, 6):
+    for j in range(i):
+        print(i+j, end=" ")
+    print()
