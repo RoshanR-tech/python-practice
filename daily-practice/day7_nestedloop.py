a33ebecd22047540037639dfd103f513046219b9
Roshan.R <roshanleo953@gmail.com>
2026-03-22 21:18:48 +0530

    Implement number triangle patterns using nested loops
    Added nested loops to print increasing and decreasing number triangles.

--- daily-practice/day7_nestedloop.py
+++ daily-practice/day7_nestedloop.py
@@ -26,3 +26,14 @@ for i in range(1,6):
     for j in range(1,6):
         print("*", end=" ")
     print()
+
+
+for i in range(1,6):                 # Increasing number triangle (controls rows)
+    for j in range(1 , i+1):         # Prints numbers from 1 to i
+        print(j , end=" ")           # Print column number (j)
+    print()                          # Move to next row
+
+for i in range(5,0,-1):              # Decreasing number triangle (rows from 5 to 1)
+    for j in range(1 , i+1):         # Prints number i, i times
+        print(i , end=" ")           # Print row number (i)
+    print()                          # Move to next row
