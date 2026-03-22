3d22b4b69683d612c2805d78bca1dc98feba9f20
Roshan.R <roshanleo953@gmail.com>
2026-03-22 21:03:14 +0530

    Implement various star and number patterns
    Added multiple patterns of star and number printing using nested loops.

--- daily-practice/day7.py
+++ daily-practice/day7.py
@@ -14,3 +14,38 @@ for i in range(5, 0 , -1):
     for j in range(1,i+1):
         print("*", end=" ")
     print()
+
+
+for i in range(1, 6): #the outer loop runs 5 times
+    for j in range(1):
+        print("*", end=" ")
+   
+
+for i in range(1):     #the inner loop runs 5 times 
+   for j in range(1, 6):
+        print("*", end=" ")
+   
+
+for i in range(1,6):        # controls rows
+    for j in range(i):     # controls stars (increasing)
+        print("*", end=" ")
+    print()                # move to next row
+
+
+
+for i in range(1,6):        # reverse triangle 
+    for j in range(6-i):    
+        print("*", end=" ")
+    print()                
+
+
+for i in range(1,6):        
+    for j in range(1, i+1):    
+        print( j, end=" ")
+    print()                
+
+
+for i in range(1,6):          # controls rows
+    for j in range(i):        # controls how many numbers per row
+        print(i, end=" ")     # print the row number
+    print()                   # move to next line
