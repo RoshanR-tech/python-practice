4cef84528a0088da73a1a179e77cfc37707d478c
Roshan.R <roshanleo953@gmail.com>
2026-04-10 23:36:13 +0530

    Add simple calculator functions to Keyconcepts.py

--- problems/Keyconcepts.py
+++ problems/Keyconcepts.py
@@ -11,3 +11,28 @@ while guess != secret_number:
         print("Correct! You guessed it.")
     else:
         print("Wrong guess, try again.")
+
+
+
+
+# Simple Calculator
+# Concept: functions, return statement
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b != 0:
+        return a / b
+    return "Cannot divide by zero"
+
+print(add(5, 3))
+print(subtract(10, 4))
+print(multiply(2, 6))
+print(divide(8, 2))
