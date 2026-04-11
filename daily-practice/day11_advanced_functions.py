32c8321293ba93077011a5829f94a92d80709446
Roshan.R <roshanleo953@gmail.com>
2026-04-11 22:26:09 +0530

    Update day11_advanced_functions.py

--- daily-practice/day11_advanced_functions.py
+++ daily-practice/day11_advanced_functions.py
@@ -1,1 +1,13 @@
-
+def calculate(a,b):
+    addition = a+b 
+    difference = a-b
+    product = a*b
+    return addition, difference, product
+a = int(input("Enter a number: "))   
+b = int(input("Enter a number: "))   
+addition, difference, product = calculate(a, b)
+print("Sum =", addition)
+print("Difference =", difference)
+print("Product =", product)
+    
+    
