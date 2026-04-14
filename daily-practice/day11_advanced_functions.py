44eac3ad55638b95af3c921c44e57712493898b3
Roshan.R <roshanleo953@gmail.com>
2026-04-14 18:02:08 +0530

    Update day11_advanced_functions.py

--- daily-practice/day11_advanced_functions.py
+++ daily-practice/day11_advanced_functions.py
@@ -11,4 +11,14 @@ print("Sum =", addition)
 print("Difference =", difference)
 print("Product =", product)
     
+
+#Functions with and without return values
+def add_print(a,b):
+    print(a+b)
+def add_return(a,b):
+    return a+b
     
+result1 = add_print(2,2)
+result2 = add_return(2,2)
+print(result1)
+print(result2)
