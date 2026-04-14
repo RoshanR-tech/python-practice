02c5213a42217692e873b8a743509b2ab19c82ee
Roshan.R <roshanleo953@gmail.com>
2026-04-14 21:51:17 +0530

    Update day11_advanced_functions.py

--- daily-practice/day11_advanced_functions.py
+++ daily-practice/day11_advanced_functions.py
@@ -72,3 +72,44 @@ elif choice == 4:
     
 else :
     print("Invalid , enter a number in btw 1-4")    #Detailing 
+
+
+
+#using while loop to create a menu driven calculator using functions
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    if b == 0:
+        return "Cannot be divided by 0"
+    else :
+        return a/b
+
+while True:
+    print("\n___Menu___")
+    print("1.Addition")
+    print("2.Subtraction")
+    print("3.Multiplication")
+    print("4.Division")
+
+    choice = int(input("Select a function: "))
+
+
+    a = int(input("Enter a number: "))
+    b = int(input("Enter a number: "))
+        
+    if choice == 1:
+        print("Result =", add(a,b))
+    elif choice == 2:
+        print("Result =", sub(a,b))
+    elif choice == 3:
+        print("Result =", mul(a,b))
+    elif choice == 4:
+        print("Result =", div(a,b))
+        
+        break  
+    else:
+        print("Invalid, choose a number from 1-4")
