45623b6b8e036017e31dd99a417599bdcc1c0644
Roshan.R <roshanleo953@gmail.com>
2026-04-15 22:01:06 +0530

    Update day12_functions_deepdive.py

--- daily-practice/day12_functions_deepdive.py
+++ daily-practice/day12_functions_deepdive.py
@@ -1,1 +1,26 @@
+#Task 1
+def addition(a,b):
+    return a+b
+def subtraction(a,b):
+    return a-b
+def multiplication(a,b):
+    return a*b
+def division(a,b):
+    if b == 0 :
+        return "cannot be divided by 0"
+    else :
+        return a/b 
 
+
+#Print vs Return 
+def addition(a,b):
+    return a+b
+def subtraction(a,b):
+    return a-b
+def multiplication(a,b):
+    return a*b
+def division(a,b):
+    if b == 0 :
+        return "cannot be divided by 0"
+    else :
+        return a/b 
