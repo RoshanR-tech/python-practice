7132ca69177d734957719315db910d01567c8a12
Roshan.R <roshanleo953@gmail.com>
2026-04-22 22:04:20 +0530

    Update day03_loops_intro.py

--- daily-practice/day03_loops_intro.py
+++ daily-practice/day03_loops_intro.py
@@ -1,3 +1,4 @@
+#basic conditions using if 
 age = int(input("enter your age: "))
 if age >= 18 :
     print("you are an adult ")
@@ -5,8 +6,7 @@ else :
     print("you are a minor ")
 
 
-
-    age = int(input("enter your age: "))
+age = int(input("enter your age: "))
 if age >= 60 :
     print("you are a senior citizen ")
 elif age >= 18 :
