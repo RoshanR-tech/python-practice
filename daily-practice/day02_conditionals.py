a0edf97f9e17f3a0470d0ba04d00f9b7712fd13e
Roshan.R <roshanleo953@gmail.com>
2026-04-22 22:03:33 +0530

    Add basic mathematical functions to the script

--- daily-practice/day02_conditionals.py
+++ daily-practice/day02_conditionals.py
@@ -4,7 +4,7 @@ city = "bangalore"
 college = "presidency university" 
 print(f"my name is {name}",f"I AM {age} years old",f"I live in {city}",f"I am studying in {college}",sep="\n")
 
-
+#basic mathematical functions 
 a = 15
 b = 4
 print("Addition:", a + b)
