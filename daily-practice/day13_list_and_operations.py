b590e2a5ecee23610fd9267f3e192aa2477ec39b
Roshan.R <roshanleo953@gmail.com>
2026-04-25 19:58:57 +0530

    Update day13_list_and_operations.py

--- daily-practice/day13_list_and_operations.py
+++ daily-practice/day13_list_and_operations.py
@@ -1,5 +1,14 @@
-nums = [10,20,30,40,50] #creating a list of numbers
+#creating a list of numbers
+nums = [10,20,30,40,50] 
 print(nums[0])
 print(nums[-1])
 nums[2]=100
+print(nums)
+
+#list methods
+nums = [1,2,3]
+nums.append(4)
+nums.insert(1 , 10) 
+nums.remove(2)
+nums.pop(-1)
 print(nums)
