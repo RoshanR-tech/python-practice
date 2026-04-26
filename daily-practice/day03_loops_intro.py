fb9b2daa1f3dcd4f352ff7147f9693e2e339118f
Roshan.R <roshanleo953@gmail.com>
2026-04-26 21:50:24 +0530

    Update age messages for voting eligibility

--- daily-practice/day03_loops_intro.py
+++ daily-practice/day03_loops_intro.py
@@ -1,9 +1,9 @@
 #basic conditions using if 
 age = int(input("enter your age: "))
 if age >= 18 :
-    print("you are an adult ")
+    print("you are an adult , eligible to vote ")
 else :
-    print("you are a minor ")
+    print("you are a minor , Not eligible to vote ")
 
 
 age = int(input("enter your age: "))
