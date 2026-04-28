759ec1d32dd6bcd73efd4c938b571e3b32edacad
Roshan.R <roshanleo953@gmail.com>
2026-04-28 20:22:08 +0530

    Add comments to clarify print functions

--- daily-practice/day01_basics_variables_io.py
+++ daily-practice/day01_basics_variables_io.py
@@ -3,7 +3,7 @@ name = input("Enter your name: ")
 age = input("Enter your age: ")
 city = input("Enter your city: ")
 college = input("Enter your college: ")
-
+#print functions
 print("\n--- Your Details ---")
 print("Name:", name)
 print("Age:", age)
