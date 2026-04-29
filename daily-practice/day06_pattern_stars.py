871dafa475ff2a716c2668b861f9ffcdc4b2668d
Roshan.R <roshanleo953@gmail.com>
2026-04-29 21:46:22 +0530

    Update day06_pattern_stars.py

--- daily-practice/day06_pattern_stars.py
+++ daily-practice/day06_pattern_stars.py
@@ -15,3 +15,10 @@ for i in range (1,5):
     for j in range(1,i+1):
         print("*" , end=" ")
     print()
+
+
+#reverse star pattern
+for i in range (6,0,-1):
+    for j in range(1,i+1):
+        print("*" , end=" ")
+    print()
