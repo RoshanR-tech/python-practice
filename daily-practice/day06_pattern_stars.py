1c497e5afda62d17b5001adf4e50b6bd9b3b0776
Roshan.R <roshanleo953@gmail.com>
2026-04-29 21:58:48 +0530

    Update day06_pattern_stars.py

--- daily-practice/day06_pattern_stars.py
+++ daily-practice/day06_pattern_stars.py
@@ -10,15 +10,18 @@ while n>=1:
     print(n)
     n = n-1 
 
-#rectangular star pattern 
+#triangular star pattern 
 for i in range (1,5):
     for j in range(1,i+1):
         print("*" , end=" ")
     print()
 
 
-#reverse rectangular star pattern
+#reverse triangular star pattern
 for i in range (6,0,-1):
     for j in range(1,i+1):
         print("*" , end=" ")
     print()
+
+
+#square 
