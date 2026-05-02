9a7c0c9c8dc6be97c02dfeac4242c3b0e1a8e065
Roshan.R <roshanleo953@gmail.com>
2026-05-02 22:12:39 +0530

    Invert even/odd logic in number printing
    Modified logic to print 'even' for odd numbers and vice versa.

--- daily-practice/day05(2).py
+++ daily-practice/day05(2).py
@@ -4,3 +4,11 @@ for i in range(1,10):
         print(f"even {i}")
     else :
         print(f"odd {i}")
+
+
+#same as the above with a small logic change 
+for i in range(1,10):
+    if i%2!=0 :
+        print(f"even {i}")
+    else :
+        print(f"odd {i}")
