8e00812e9af80ce4104a25231056fb6846261e3d
Roshan.R <roshanleo953@gmail.com>
2026-05-05 21:08:56 +0530

    Add comment to clarify default argument in student function
    Added a comment explaining the default argument for course.

--- daily-practice/day09_arguments.py
+++ daily-practice/day09_arguments.py
@@ -1,5 +1,5 @@
 #Default Arguments
-def student(name , course="python"):
+def student(name , course="python"):             #default of course would be python until it is changed 
     print(f"Hello {name} is learning {course}")
 student("Roshan.R")
 student("ankit","java")
