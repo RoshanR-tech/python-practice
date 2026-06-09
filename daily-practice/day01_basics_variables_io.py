24aea6977d8a95cf8fdee2d58d4894fe187ccff3
Roshan.R <roshanleo953@gmail.com>
2026-06-09 21:43:45 +0530

    Add country input and print to details output

--- daily-practice/day01_basics_variables_io.py
+++ daily-practice/day01_basics_variables_io.py
@@ -2,12 +2,13 @@
 name = input("Enter your name: ")
 age = input("Enter your age: ")
 city = input("Enter your city: ")
+country = input("Enter your country: ")
 #print functions
 print("\n--- Your Details ---")
 print("Name:", name)
 print("Age:", age)
 print("city:", city)
-
+print("country:", country)
 
 
 
