7d21976bf7a6550820c42c94e22b95f1e505d8b8
Roshan.R <roshanleo953@gmail.com>
2026-06-17 21:51:49 +0530

    Update day01_basics_variables_io.py

--- daily-practice/day01_basics_variables_io.py
+++ daily-practice/day01_basics_variables_io.py
@@ -2,11 +2,13 @@
 name = input("Enter your name: ")
 age = input("Enter your age: ")
 city = input("Enter your city: ")
+counrtry = input("enter your country: ")
 #print functions
 print("\n--- Your Details ---")
 print("Name:", name)
 print("Age:", age)
 print("city:", city)
+print("country:",country)
 
 
 
