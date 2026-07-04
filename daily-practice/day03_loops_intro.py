869251abc9043c1cf0a5253757e774749eefed96
Roshan.R <roshanleo953@gmail.com>
2026-07-04 20:45:25 +0530

    Update day03_loops_intro.py

--- daily-practice/day03_loops_intro.py
+++ daily-practice/day03_loops_intro.py
@@ -1,6 +1,6 @@
 #basic conditions using if 
 age = int(input("enter your age: "))
-if age >= 18 :
+if age >= 20 :
     print("you are an adult , eligible to vote ")
 else :
     print("you are a minor , Not eligible to vote ")
