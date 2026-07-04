8dbb98b732c2e6c52d1a5c31b89666775330359f
Roshan.R <roshanleo953@gmail.com>
2026-07-04 20:45:40 +0530

    Update voting age eligibility check from 20 to 18

--- daily-practice/day03_loops_intro.py
+++ daily-practice/day03_loops_intro.py
@@ -1,6 +1,6 @@
 #basic conditions using if 
 age = int(input("enter your age: "))
-if age >= 20 :
+if age >= 18 :
     print("you are an adult , eligible to vote ")
 else :
     print("you are a minor , Not eligible to vote ")
