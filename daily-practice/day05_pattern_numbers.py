70fdd8e4041912cbd170bb3da951d036054ba458
Roshan.R <roshanleo953@gmail.com>
2026-07-06 22:18:07 +0530

    Update day05_pattern_numbers.py

--- daily-practice/day05_pattern_numbers.py
+++ daily-practice/day05_pattern_numbers.py
@@ -3,5 +3,5 @@ for i in range(1, 11):
     print(i)
 
 #Reverse format
-for i in range(9, 0, -1): #one number backword every loop
+for i in range(10, 0, -1): #one number backword every loop
     print(i)
