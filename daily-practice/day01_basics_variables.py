529b3fff37455b59c85017d61daf4163f08b27f4
Roshan.R <roshanleo953@gmail.com>
2026-07-07 22:22:57 +0530

    Remove zip code input prompt
    Removed zip code input from user details.

--- daily-practice/day01_basics_variables.py
+++ daily-practice/day01_basics_variables.py
@@ -3,7 +3,6 @@ name = input("Enter your name: ")
 age = input("Enter your age: ")
 city = input("Enter your city: ")
 country = input("Enter your country: ")
-zip_code = input("enter your zip code:")
 #print functions
 print("\n--- Your Details ---")
 print("Name:", name)
