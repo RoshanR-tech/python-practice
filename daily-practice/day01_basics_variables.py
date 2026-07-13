cc4b1474a615a27ee0c3c14b1f41c24585d5cd81
Roshan.R <roshanleo953@gmail.com>
2026-07-13 22:09:15 +0530

    Update day01_basics_variables.py

--- daily-practice/day01_basics_variables.py
+++ daily-practice/day01_basics_variables.py
@@ -3,6 +3,7 @@ name = input("Enter your name: ")
 age = input("Enter your age: ")
 city = input("Enter your city: ")
 country = input("Enter your country: ")
+zip_code = input("Enter your Z: ")
 #print functions
 print("\n--- Your Details ---")
 print("Name:", name)
