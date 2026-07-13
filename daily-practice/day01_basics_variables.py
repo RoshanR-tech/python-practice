533c294ba992ecd01f13cb6b01ee4c9a0d16400a
Roshan.R <roshanleo953@gmail.com>
2026-07-13 22:09:43 +0530

    Update prompt for zip code input

--- daily-practice/day01_basics_variables.py
+++ daily-practice/day01_basics_variables.py
@@ -3,12 +3,13 @@ name = input("Enter your name: ")
 age = input("Enter your age: ")
 city = input("Enter your city: ")
 country = input("Enter your country: ")
-zip_code = input("Enter your Z: ")
+zip_code = input("Enter your Zip code: ")
 #print functions
 print("\n--- Your Details ---")
 print("Name:", name)
 print("Age:", age)
 print("Country:",country)
+
 
 
 
