b2435da3896404f864832b8ccf43d32e8c841e3a
Roshan.R <roshanleo953@gmail.com>
2026-07-31 22:02:00 +0530

    Update day02_conditionals.py

--- daily-practice/day02_conditionals.py
+++ daily-practice/day02_conditionals.py
@@ -2,7 +2,7 @@ name = "Roshan.R"
 age = 20
 city = "bangalore"
 college = "presidency university" 
-print(f"my name is {name}",f"I AM {age} years old",f"I live in {city}",f"I am studying in {college}",sep="\n")
+print(f"my name is {name}\nmy age is {age}\nmy city is {city}\nmy college is {college}")
 
 #basic mathematical functions 
 a = 20
