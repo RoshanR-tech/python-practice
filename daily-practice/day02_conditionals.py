fc46c49dc294806e1c92d03a0a9851ff0a8346de
Roshan.R <roshanleo953@gmail.com>
2026-08-19 19:43:13 +0530

    Update day02_conditionals.py

--- daily-practice/day02_conditionals.py
+++ daily-practice/day02_conditionals.py
@@ -5,8 +5,8 @@ college = "presidency university"
 print(f"my name is {name}\nmy age is {age}\nmy city is {city}\nmy college is {college}")
 
 #basic mathematical functions 
-a = 20
-b = 5
+a = 16
+b = 4
 print("Addition:", a + b)
 print("Subtraction:", a - b)
 print("Multiplication:", a * b)
